79b2c984bc5310d845d7408f348f9aa078c43f8a
Mariana13097 <marizaco@uninpahu.edu.co>
2025-04-23 20:37:10 -0500

    Add files via upload

---
+++ Buclewhile Mari .java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package buclewhilemari;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class buclewhilemari {
+
+   
+    public static void main(String[] args) {
+        int contador = 25;
+        
+        while (contador>0){
+            System.out.println(contador);
+            
+            contador = contador -1;
+        }
+       
+    }
+    
+}
